92c58b1a85913ae55d4687bd11e06d5cd9fb09ed
pirsonxyz <pirson@pirson.xyz>
2025-03-07 23:42:45 -0400

    useless comment

--- configuration.nix
+++ configuration.nix
@@ -43,7 +43,7 @@
     LC_TIME = "en_US.UTF-8";
   };
   programs.nix-ld.enable = true;
-
+  #useless comment
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
