4ea763423033bd7cca333e7957195ac359fdc1c9
pirsonxyz <pirson@pirson.xyz>
2025-04-13 00:32:41 -0400

    formatting

--- configuration.nix
+++ configuration.nix
@@ -56,18 +56,18 @@
   };
   services.desktopManager.plasma6.enable = true;
   programs.steam = {
-  enable = true;
-  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-};
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
- programs.ssh = {
-    startAgent = true; 
+  programs.ssh = {
+    startAgent = true;
   };
 
   # Enable CUPS to print documents.
--- modules/git.nix
+++ modules/git.nix
@@ -1,14 +1,12 @@
 {
-programs.git = {
+  programs.git = {
     enable = true;
     userName = "pirsonxyz";
-    userEmail = "pirson@pirson.xyz"; 
- signing = {
-     
+    userEmail = "pirson@pirson.xyz";
+    signing = {
       key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILj7z/frooN4KqCugg80Y1uZGn4E6bobjQeIs7RMZXDD pirson@pirson.xyz";
       signByDefault = true;
       format = "ssh";
     };
   };
 }
-
